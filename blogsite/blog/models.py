a701fcde1262fc2062e58c59a6da1fe06bb20ad7
nourdevart <nourdevart@gmail.com>
2026-02-24 17:13:21 +0200

    200~Module 2 - Using Canonical URLs For Models

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.utils import timezone
 from django.contrib.auth.models import User 
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -30,3 +31,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
